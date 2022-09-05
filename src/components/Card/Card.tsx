f973a2b1a7287a42cbed71de81138c66c67fdf92
tigerabrodi <tigerabrodi@gmail.com>
2022-09-05 08:59:00 +0200

    feat: implement textarea changes and focus for cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,9 @@
 import type { CardType } from '../../store'
 
+import { createEffect, createSignal } from 'solid-js'
+
+import { setCards } from '../../store'
+import { cards } from '../../store'
 import { formatOrdinals } from '../../utils'
 import './Card.css'
 
@@ -8,7 +12,29 @@ type CardProps = {
 }
 
 export function Card(props: CardProps) {
+  const [cardText, setCardText] = createSignal(props.card.text)
+
+  // With this format we can give the textareas a unique accessible name.
   const cardNumberWithOrdinal = formatOrdinals(props.card.id + 1)
+
+  let textareaElement: HTMLTextAreaElement | undefined
+
+  createEffect(() => {
+    const isLastCard = props.card.id === cards[cards.length - 1].id
+    if (textareaElement && isLastCard) {
+      textareaElement.focus()
+    }
+  })
+
+  function handleTextareaChange(
+    event: InputEvent & {
+      currentTarget: HTMLTextAreaElement
+      target: Element
+    }
+  ) {
+    setCardText((event.target as HTMLTextAreaElement).value)
+    setCards(props.card.id, { text: cardText() })
+  }
 
   return (
     <div
@@ -19,8 +45,11 @@ export function Card(props: CardProps) {
       }}
     >
       <textarea
+        ref={textareaElement}
         aria-label={`Edit text for ${cardNumberWithOrdinal} card`}
         class="card__textarea"
+        value={cardText()}
+        onInput={handleTextareaChange}
       />
     </div>
   )
