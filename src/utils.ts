a8eb5087d4e9ee205d578dadefc230c2e732df81
tigerabrodi <tigerabrodi@gmail.com>
2022-09-05 07:55:35 +0200

    utils to add ordinals

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,20 @@
+const pluralRule = new Intl.PluralRules('en-US', {
+  type: 'ordinal',
+})
+
+const suffixes = new Map([
+  ['one', 'st'],
+  ['two', 'nd'],
+  ['few', 'rd'],
+  ['other', 'th'],
+])
+
+export function formatOrdinals(number: number) {
+  const rule = pluralRule.select(number)
+  const suffix = suffixes.get(rule)
+  return `${number}${suffix}`
+}
+
 export function getRandomId() {
   return Math.floor(
     Math.pow(10, 5 - 1) + Math.random() * 9 * Math.pow(10, 5 - 1)
