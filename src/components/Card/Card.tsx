6bf9b709c6f4713776d76651f69e9f8de144c2b5
tigerabrodi <tigerabrodi@gmail.com>
2022-09-05 10:49:18 +0200

    fix: handle textarea focus on refresh

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -14,7 +14,7 @@ type CardProps = {
 export function Card(props: CardProps) {
   const [cardText, setCardText] = createSignal(props.card.text)
   const [isDragging, setIsDragging] = createSignal(false)
-  const [isTextareaFocused, setIsTextareaFocused] = createSignal(true)
+  const [isTextareaFocused, setIsTextareaFocused] = createSignal(false)
 
   // With this format we can give the textareas a unique accessible name.
   const cardNumberWithOrdinal = formatOrdinals(props.card.id + 1)
@@ -23,7 +23,7 @@ export function Card(props: CardProps) {
 
   createEffect(() => {
     const isLastCard = props.card.id === cards[cards.length - 1].id
-    if (textareaElement && isLastCard && isTextareaFocused()) {
+    if (textareaElement && props.card.isNew && isLastCard) {
       textareaElement.focus()
     }
   })
@@ -59,6 +59,11 @@ export function Card(props: CardProps) {
     setIsTextareaFocused(true)
   }
 
+  function handleTextareBlur() {
+    setIsTextareaFocused(false)
+    setCards(props.card.id, { isNew: false })
+  }
+
   return (
     <div
       class="card"
@@ -79,7 +84,7 @@ export function Card(props: CardProps) {
         }}
         disabled={!isTextareaFocused()}
         onFocus={() => setIsTextareaFocused(true)}
-        onBlur={() => setIsTextareaFocused(false)}
+        onBlur={handleTextareBlur}
         aria-label={`Edit text for ${cardNumberWithOrdinal} card`}
         class="card__textarea"
         value={cardText()}
